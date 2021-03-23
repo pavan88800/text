a3074a62996c434f9b82f193579b43184e745cba
Pavan b <43606935+pavan88800@users.noreply.github.com>
2021-03-23 19:08:00 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,2 @@
 console.log ('hello word here');
 
-console.log ('Doctoret isdjkfsdf');
-
-console.log ('data is more important is here please make changes here ');
-
-console.log ('files is more important is here please make changes here ');
-
-console.log ('pavan pauv 123123edf');
-console.log ('pavan pauv 123123edf');
-console.log ('pavan pauv 123123edf');
-console.log ('pavan pauv 123123edf');
-console.log ('pavan pauv 123123edf');
-console.log ('pavan pauv 123123edf');
-console.log ('pavan pauv 123123edf');
