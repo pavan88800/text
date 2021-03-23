d9865e966ce75f0255c8f4e09d41f9860e7d2e3f
pavan <pavannttf29@gmail.com>
2021-03-23 19:28:02 +0530

    merge

--- main.js
+++ main.js
@@ -1,28 +1,2 @@
 console.log ('hello word here');
-
-console.log ('hello word here');
-
-console.log ('hello word here');
-
-console.log ('hello word here');
-
-console.log ('hello word here');
-
-// fsdfsdf
-
-console.log ('hello word here');
-
-console.log ('hello word here');
-
-console.log ('hello word here');
-
-console.log ('hello word here');
-
-console.log ('hello word here');
-
-console.log ('hello word here');
-
-console.log ('hello word here');
-
-console.log ('hello word here');
 // asdfasdfnew commit here
