6a23137cc8a1927304d086b67b9dafacdcc8f585
Pavan b <43606935+pavan88800@users.noreply.github.com>
2021-03-24 12:47:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,2 @@
 console.log ('hello word here');
 
-console.log ('pavan pauv 123123edf');
-
-console.log ('its master branch');
-
-console.log ('its master branch');
-console.log ('its master branch');
-console.log ('its master branch');
-console.log ('its master branch');
-console.log ('its master branch');
